b63aca882e556958f1eb377e78a24162b71ede2c
rob <id2go4@gmail.com>
2018-07-10 16:58:47 +0200

    Modified PetContract
    Added Uri import
    added:
     private PetContract() {
        }
            public static final String CONTENT_AUTHORITY = "eu.id2go.pets"; // ...id2go... in stead of: com.example.android.pets
                public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
                    public static final String PATH_PETS = "pets";
            public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
    
    Modified PetProvider:
    imports: ContentUris, URIMatcher, SQLiteDatabase, PetsEntry
    
        private static final int PETS = 100;
        private static final int PET_ID = 101;
        private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
        static {
            sUriMatcher.addURI("pets", "pets", PETS);           // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
            sUriMatcher.addURI("pets", "pets/#", PET_ID);       // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
    //        sUriMatcher.addURI("pets", "pets/#", PET_NAME);     // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_NAME);
    //        sUriMatcher.addURI("pets", "pets/#", PET_BREED);    // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_BREED);
    //        sUriMatcher.addURI("pets", "pets/#", PET_GENDER);   // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_GENDER);
    //        sUriMatcher.addURI("pets", "pets/#", PET_WEIGHT);   // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_WEIGHT);
    
        }
    
    Modified Cursor query:
            SQLiteDatabase database = mDbHelper.getReadableDatabase();
            Cursor cursor;
    
            int match = sUriMatcher.match(uri);
            switch (match) {
                case PETS:
                cursor = database.query(PetsEntry.TABLE_NAME, projection, selection,
                                        selectionArgs, null, null, sortOrder);
                                break;
                            case PET_ID:
                            selection = PetsEntry._ID + "=?";
                                            selectionArgs = new String[] {
                                                   String.valueOf(ContentUris.parseId(uri))
                                            };
                                            cursor = database.query(PetsEntry.TABLE_NAME, projection, selection,
                                                                    selectionArgs, null, null, sortOrder);
                                                            break;
                                                        default:
                                                            throw new IllegalArgumentException("Cannot query unknown URI " + uri);
                                                    }
                                                    return cursor;
                                                }

--- app/src/main/java/eu/id2go/pets/data/PetContract.java
+++ app/src/main/java/eu/id2go/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package eu.id2go.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,12 +8,59 @@ import android.provider.BaseColumns;
  * Outer class named: BlankContract
  * Inner class named: BlankEntry that implements BaseColumns for each table in the database
  * String constants for each of the headings in the database
+ * Data related CONSTANTS are reusable components that do not change and that are for purpose of
+ * identifacation WRITEN IN CAPS
  */
 public final class PetContract {
 
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.
+     * A convenient string to use for the content authority is the package name for the app, which
+     * is guaranteed to be unique on the device.
+     */
+    public static final String CONTENT_AUTHORITY = "eu.id2go.pets"; // ...id2go... in stead of: com.example.android.pets
+    /**
+     * BASE_CONTENT_URI
+     * Concatenate the CONTENT_AUTHORITY constant with the scheme “content://”
+     * & create the BASE_CONTENT_URI which will be shared by every URI associated with PetContract:
+     * "content://" + CONTENT_AUTHORITY
+     * In order to make this a usable URI, use the parse method which takes in a URI string and returns a Uri.
+     * <p>
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    /**
+     * PATH_TableName
+     * This constants stores the path for each of the tables which will be appended to the base content URI.
+     * <p>
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
+    private PetContract() {
+    }
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
     public static abstract class PetsEntry implements BaseColumns {
 
-//        private PetContract() {}
+        /**
+         * CONTENT_URI
+         * Lastly, inside each of the Entry classes in the contract, create a full URI for the class
+         * as a constant called CONTENT_URI. The Uri.withAppendedPath() method appends the BASE_CONTENT_URI
+         * (which contains the scheme and the content authority) to the path segment.
+         * <p>
+         * The content URI to access the pet data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
 
         public static final String TABLE_NAME = "pets";
 
@@ -29,5 +77,6 @@ public final class PetContract {
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
     }
+
 }
 
--- app/src/main/java/eu/id2go/pets/data/PetProvider.java
+++ app/src/main/java/eu/id2go/pets/data/PetProvider.java
@@ -1,9 +1,14 @@
 package eu.id2go.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+
+import eu.id2go.pets.data.PetContract.PetsEntry;
 
 /**
  * {@link ContentProvider} for Pets app.
@@ -15,6 +20,40 @@ public class PetProvider extends ContentProvider {
      */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
 
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
+    private static final int PETS = 100;
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
+    private static final int PET_ID = 101;
+//    private static final int PET_NAME = 102;
+//    private static final int PET_BREED = 103;
+//    private static final int PET_GENDER = 104;
+//    private static final int PET_WEIGHT = 105;
+
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+        sUriMatcher.addURI("pets", "pets", PETS);           // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+        sUriMatcher.addURI("pets", "pets/#", PET_ID);       // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
+//        sUriMatcher.addURI("pets", "pets/#", PET_NAME);     // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_NAME);
+//        sUriMatcher.addURI("pets", "pets/#", PET_BREED);    // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_BREED);
+//        sUriMatcher.addURI("pets", "pets/#", PET_GENDER);   // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_GENDER);
+//        sUriMatcher.addURI("pets", "pets/#", PET_WEIGHT);   // sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS +"/#", PET_WEIGHT);
+
+    }
 
     /**
      * Database helper object
@@ -39,6 +78,49 @@ public class PetProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // For the Pets code, query the pets table directly with the given projection,
+                // selection, selection arguments, and sort order.
+                // The cursor could contain multiple rows of the pets table.
+                cursor = database.query(PetsEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            case PET_ID:
+                // For the PET_ID code, extract out the _ID from the URI.
+                // For an example URI such as " content://com.example.android.pets/pets/3",
+                // the selection will be " _id=?" and the selection argument wil be a
+                // String array containing the actual _ID of 3 in this case.
+                //
+                //For every "?" in the selection, we need to have an element in the selection arguments
+                // that will fill in the "?". Since we have 1 question mark in the selection,
+                // we hae 1 string in the selection arguments String array.
+                selection = PetsEntry._ID + "=?";
+                selectionArgs = new String[]{
+                        String.valueOf(ContentUris.parseId(uri))
+                };
+                // This will perform a query on the pets table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(PetsEntry.TABLE_NAME, projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
+    }
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     */
+    @Override
+    public String getType(Uri uri) {
         return null;
     }
 
@@ -66,11 +148,5 @@ public class PetProvider extends ContentProvider {
         return 0;
     }
 
-    /**
-     * Returns the MIME type of data for the content URI.
-     */
-    @Override
-    public String getType(Uri uri) {
-        return null;
-    }
+
 }
